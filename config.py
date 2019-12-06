44d31d8054923086b69ce6fa993af3524eba0de2
Anu Srivastava <asrivasts@gmail.com>
2019-12-06 16:06:33 -0500

    LastFM data

---
+++ config.py
@@ -0,0 +1,7 @@
+# LastFM API Key
+lastfm_api_key = "1ead87eb698a934e8b663e0491f8531b"
+lastfm_api_secret = "e44621696d71fddac3f7115868b412cc"
+
+# Spotify API Key
+anu_client_id = "e2ead1b438e14d3382d02c7157f916f9"
+anu_secret_key = "6d38c6e5c9194a1988e5ad36b46f0dbe"
